3105af4da8cd579bf9b018c83e0e5a314f92da82
Ericwyn <16485562+Ericwyn@users.noreply.github.com>
2024-06-18 15:52:09 +0800

    feat: support customs dns hosts

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ docker run --privileged -d \
     - 需要确保 `/myconfig/openvpn` 文件夹下存在 `vpn.ovpn` 配置文件
     - 暴露本地 `18080` 端口作为 http 代理端口
     - 环境变量 `SSH_ROOT_PW` 为 root 登录密码
+    - 环境变量 `DNS_HOST` 可自定义 HOST
 
 
 ### 定时重启
@@ -48,3 +49,16 @@ crontab -e
 # cd 到项目文件夹
 sudo docker build -t ericwyn/ovpn-proxy:latest .
 ```
+
+
+## 容器内网络问题排查
+使用以下命令安装一些常用工具
+
+用以排查可能出现的一些异常问题
+```
+apt update
+
+apt install dnsutils iputils-ping net-tools -y
+
+```
+
--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# 检查环境变量 DNS_HOST 是否设置，如果设置了，就配置 DNS
+if [ -n "$DNS_HOST" ]; then
+    echo "==== Try Set DNS Conf ===="
+    echo "Checking DNS configuration..."
+    if ! grep -q "nameserver $DNS_HOST" /etc/resolv.conf; then
+        echo "Adding DNS server $DNS_HOST to /etc/resolv.conf"
+        echo "nameserver $DNS_HOST" >> /etc/resolv.conf
+    else
+        echo "DNS server $DNS_HOST already configured in /etc/resolv.conf"
+    fi
+    echo "."
+    echo "."
+    echo "."
+fi
+
 # 检查环境变量 SSH_ROOT_PW 是否设置，如果设置了，就使用该密码
 if [ -n "$SSH_ROOT_PW" ]; then
+    echo "==== Try Set SSH Conf ===="
     echo "Setting root password..."
     echo "root:$SSH_ROOT_PW" | chpasswd
     echo "Setting root password success"
